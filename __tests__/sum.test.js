64751ba6bf2e6732a80663f1b0d270e184fd9547
sachalath <sacha.lathuilliere@esme.fr>
2025-11-13 14:51:39 +0100

    Rajout de commentaires

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,7 @@ import { sum } from '../src/sum.js';
 describe('sum', () => {
  test('additionne deux nombres', () => {
  expect(sum(2, 3)).toBe(5);
- });
+ }); //commentaire ajouté
  test('gère les nombres négatifs', () => {
  expect(sum(-2, 5)).toBe(3);
  });
